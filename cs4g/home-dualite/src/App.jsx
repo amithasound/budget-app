7028da8881e0bb8b3251b934ac3d65f4e0f45fee
amithasound <70346133+amithasound@users.noreply.github.com>
2024-03-25 15:46:47 -0400

    generated code

---
+++ cs4g/home-dualite/src/App.jsx
@@ -0,0 +1,404 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import "./animation.css";
+import runAnimations, { allLinks, allFunctions } from "./scripts";
+const App = () => {
+  useEffect(() => {
+    runAnimations();
+  }, []);
+  return (
+    <div className="parent-div">
+      <div className="home-1-6440 pos-abs" id="id-15">
+        <div className="hello-julia-1-3900 pos-abs" id="id-111">
+          <span className="hello-julia-1-3900-0">{"Hello, Julia! "}</span>
+        </div>
+        <div className="welcome-to-budg-1-85904 pos-abs" id="id-252">
+          <span className="welcome-to-budg-1-85904-0">
+            {"Welcome to Budget Buddy! "}
+          </span>
+        </div>
+        <div className="view-your-progr-1-42001 pos-abs" id="id-253">
+          <span className="view-your-progr-1-42001-0">
+            {"View your Progress: "}
+          </span>
+        </div>
+        <div className="ira-progress-1-535074 pos-abs" id="id-24">
+          <div className="rectangle-1-1-36295 pos-abs" id="id-23"></div>
+          <div className="rectangle-1-2-114534 pos-abs" id="id-211"></div>
+          <div className="ira-1-308722 pos-abs" id="id-115">
+            <span className="ira-1-308722-0">{"IRA: "}</span>
+          </div>
+        </div>
+        <div className="car-progress-1-60960 pos-abs" id="id-219">
+          <div className="rectangle-1-1-87584 pos-abs" id="id-220"></div>
+          <div className="rectangle-1-2-6700 pos-abs" id="id-221"></div>
+          <div className="car-1-10816 pos-abs" id="id-222">
+            <span className="car-1-10816-0">{"Car:"}</span>
+          </div>
+        </div>
+        <div className="c-401k-progress-1-518840 pos-abs" id="id-215">
+          <div className="rectangle-1-1-778207 pos-abs" id="id-216"></div>
+          <div className="rectangle-1-2-266409 pos-abs" id="id-217"></div>
+          <div className="c-401k-1-563539 pos-abs" id="id-218">
+            <span className="c-401k-1-563539-0">{"401K: "}</span>
+          </div>
+        </div>
+        <div className="investing-101-b-1-211899 pos-abs" id="id-4864">
+          <div className="investment-acco-1-591360 pos-abs" id="id-4850">
+            <span className="investment-acco-1-591360-0">
+              {"Investment Accounts 101 ->"}
+            </span>
+          </div>
+          <div className="investment-acco-1-262808 pos-abs" id="id-57104">
+            <div className="rectangle-1-1-333670 pos-abs" id="id-57105"></div>
+          </div>
+        </div>
+        <div className="frame-51-0-28480 pos-abs" id="id-105217">
+          <div className="navigation-bar-1-320933 pos-abs" id="id-105218">
+            <div className="rectangle-2-1-219760 pos-abs" id="id-105219"></div>
+            <div className="profile-1-16448 pos-abs" id="id-105235">
+              <div className="freeprofile1439-1-563550 pos-abs" id="id-105236">
+                <div
+                  className="nodeBg-105236 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                  id="id-bg-105236"
+                >
+                  {" "}
+                </div>
+              </div>
+            </div>
+            <div className="wrench-1-43470 pos-abs" id="id-105225">
+              <div className="wrench-1-146335 pos-abs" id="id-105226">
+                <div className="ellipse-1-1-7380 pos-abs" id="id-105227"></div>
+                <div
+                  className="rectangle-9-1-484288 pos-abs"
+                  id="id-105228"
+                ></div>
+                <div
+                  className="rectangle-10-1-162408 pos-abs"
+                  id="id-105229"
+                ></div>
+              </div>
+            </div>
+            <div className="home-1-213303 pos-abs" id="id-105220">
+              <div className="home-icon-1-66150 pos-abs" id="id-105221">
+                <div
+                  className="rectangle-7-1-143295 pos-abs"
+                  id="id-105222"
+                ></div>
+                <div
+                  className="rectangle-8-1-846060 pos-abs"
+                  id="id-105223"
+                ></div>
+                <div className="polygon-1-1-352823 pos-abs" id="id-105224">
+                  <div
+                    className="nodeBg-105224 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-105224"
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="stock-1-346944 pos-abs" id="id-105230">
+              <div className="stock-1-392832 pos-abs" id="id-105231">
+                <div className="line-15-1-252756 pos-abs" id="id-105233"></div>
+                <div className="line-13-1-124926 pos-abs" id="id-105232">
+                  <div
+                    className="nodeBg-105232 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-105232"
+                  ></div>
+                </div>
+                <div className="line-14-1-320712 pos-abs" id="id-105234">
+                  <div
+                    className="nodeBg-105234 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-105234"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="back-arrow1-0-18942 pos-abs" id="id-6290">
+          <div className="rectangle-1-1-267665 pos-abs" id="id-6291"></div>
+          <div className="arrow-6-1-32868 pos-abs" id="id-6292">
+            <div
+              className="nodeBg-6292 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+              id="id-bg-6292"
+            ></div>
+          </div>
+        </div>
+        <div className="frame-41-0-62776 pos-abs" id="id-10110">
+          <div className="navigation-bar-1-369462 pos-abs" id="id-3712">
+            <div className="rectangle-2-1-98728 pos-abs" id="id-223"></div>
+            <div className="profile-1-212232 pos-abs" id="id-94231">
+              <div className="freeprofile1439-1-83848 pos-abs" id="id-3027">
+                <div
+                  className="nodeBg-3027 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                  id="id-bg-3027"
+                >
+                  {" "}
+                </div>
+              </div>
+            </div>
+            <div className="wrench-1-536568 pos-abs" id="id-94169">
+              <div className="wrench-1-8094 pos-abs" id="id-2511">
+                <div className="ellipse-1-1-211500 pos-abs" id="id-258"></div>
+                <div className="rectangle-9-1-867390 pos-abs" id="id-259"></div>
+                <div
+                  className="rectangle-10-1-570568 pos-abs"
+                  id="id-2510"
+                ></div>
+              </div>
+            </div>
+            <div className="home-1-474880 pos-abs" id="id-10111">
+              <div className="home-icon-1-152832 pos-abs" id="id-257">
+                <div className="rectangle-7-1-803768 pos-abs" id="id-254"></div>
+                <div className="rectangle-8-1-13767 pos-abs" id="id-255"></div>
+                <div className="polygon-1-1-69088 pos-abs" id="id-256">
+                  <div
+                    className="nodeBg-256 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-256"
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="stock-1-669810 pos-abs" id="id-94230">
+              <div className="stock-1-79329 pos-abs" id="id-2516">
+                <div className="line-15-1-532042 pos-abs" id="id-2514"></div>
+                <div className="line-13-1-182206 pos-abs" id="id-2512">
+                  <div
+                    className="nodeBg-2512 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-2512"
+                  ></div>
+                </div>
+                <div className="line-14-1-157437 pos-abs" id="id-2513">
+                  <div
+                    className="nodeBg-2513 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-2513"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section-21-0-531182 pos-abs" id="id-102">
+          <div className="account-types-1-169260 pos-abs" id="id-1010">
+            <div className="learn-about-som-1-549310 pos-abs" id="id-4561">
+              <span className="learn-about-som-1-549310-0">
+                {"Learn about some popular investment accounts! "}
+              </span>
+            </div>
+            <div className="c-529-1-476560 pos-abs" id="id-1029">
+              <div
+                className="c-529--rectangle-1-474859 pos-abs"
+                id="id-1030"
+              ></div>
+              <div className="c-529-1-1936 pos-abs" id="id-1031">
+                <span className="c-529-1-1936-0">
+                  {"529 "} <br />{" "}
+                </span>
+              </div>
+              <div className="arrow-1-1-167936 pos-abs" id="id-1032">
+                <div
+                  className="nodeBg-1032 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-1032"
+                ></div>
+              </div>
+            </div>
+            <div className="roth-401k-1-42300 pos-abs" id="id-1023">
+              <div
+                className="roth-401k-recta-1-565056 pos-abs"
+                id="id-1018"
+              ></div>
+              <div className="roth-401k-1-18048 pos-abs" id="id-1014">
+                <span className="roth-401k-1-18048-0">{"Roth 401K"}</span>
+              </div>
+              <div className="arrow-2-1-78678 pos-abs" id="id-1034">
+                <div
+                  className="nodeBg-1034 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-1034"
+                ></div>
+              </div>
+            </div>
+            <div className="roth-ira-1-256003 pos-abs" id="id-1024">
+              <div
+                className="roth-ira-rectan-1-787710 pos-abs"
+                id="id-1019"
+              ></div>
+              <div className="roth-ira-1-531630 pos-abs" id="id-1015">
+                <span className="roth-ira-1-531630-0">{"Roth IRA"}</span>
+              </div>
+              <div className="arrow-1-1-291312 pos-abs" id="id-1036">
+                <div
+                  className="nodeBg-1036 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-1036"
+                ></div>
+              </div>
+            </div>
+            <div className="c-401k-1-83049 pos-abs" id="id-1022">
+              <div
+                className="c-401k-rectangle-1-13545 pos-abs"
+                id="id-1017"
+              ></div>
+              <div className="c-401k-1-153986 pos-abs" id="id-1012">
+                <span className="c-401k-1-153986-0">{"401K"}</span>
+              </div>
+              <div className="arrow-1-1-64638 pos-abs" id="id-1035">
+                <div
+                  className="nodeBg-1035 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-1035"
+                ></div>
+              </div>
+            </div>
+            <div className="ira-1-612219 pos-abs" id="id-1021">
+              <div className="ira-rectangle-1-75492 pos-abs" id="id-1016"></div>
+              <div className="ira-1-46004 pos-abs" id="id-1013">
+                <span className="ira-1-46004-0">{"IRA"}</span>
+              </div>
+              <div className="arrow-1-1-276750 pos-abs" id="id-1033">
+                <div
+                  className="nodeBg-1033 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-1033"
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div className="navigation-bar-1-492507 pos-abs" id="id-3745">
+            <div className="rectangle-2-1-74632 pos-abs" id="id-3746"></div>
+            <div className="freeprofile1439-1-113703 pos-abs" id="id-3759">
+              <div
+                className="nodeBg-3759 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                id="id-bg-3759"
+              >
+                {" "}
+              </div>
+            </div>
+            <div className="wrench-1-397132 pos-abs" id="id-3751">
+              <div className="ellipse-1-1-756028 pos-abs" id="id-3752"></div>
+              <div className="rectangle-9-1-350583 pos-abs" id="id-3753"></div>
+              <div className="rectangle-10-1-143184 pos-abs" id="id-3754"></div>
+            </div>
+            <div className="home-icon-1-448128 pos-abs" id="id-3747">
+              <div className="rectangle-7-1-279524 pos-abs" id="id-3748"></div>
+              <div className="rectangle-8-1-155904 pos-abs" id="id-3749"></div>
+              <div className="polygon-1-1-52731 pos-abs" id="id-3750">
+                <div
+                  className="nodeBg-3750 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-3750"
+                ></div>
+              </div>
+            </div>
+            <div className="stock-1-427427 pos-abs" id="id-3755">
+              <div className="line-15-1-83018 pos-abs" id="id-3757"></div>
+              <div className="line-13-1-48158 pos-abs" id="id-3756">
+                <div
+                  className="nodeBg-3756 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-3756"
+                ></div>
+              </div>
+              <div className="line-14-1-1218 pos-abs" id="id-3758">
+                <div
+                  className="nodeBg-3758 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                  id="id-bg-3758"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="section-41-0-36290 pos-abs" id="id-6252">
+          <div className="info-1-115299 pos-abs" id="id-6253">
+            <div className="basics-1-642818 pos-abs" id="id-6254">
+              <span className="basics-1-642818-0">{"Basics:"}</span>
+            </div>
+            <div className="basics-info-1-423604 pos-abs" id="id-6255">
+              <span className="basics-info-1-423604-0">
+                {
+                  "-A 529 plan is a tax-advantaged savings plan specifically designed to help families save for future college expenses. These plans are sponsored by states, state agencies, or educational institutions and offer a range of investment options."
+                }
+              </span>
+            </div>
+            <div className="c-529-college-sav-1-73676 pos-abs" id="id-6258">
+              <span className="c-529-college-sav-1-73676-0">
+                {"529 "} <br />{" "}
+              </span>
+              <span className="c-529-college-sav-1-73676-1">
+                {"(College Savings Plan)"}
+              </span>
+            </div>
+            <div className="frame-1-1-383786 pos-abs" id="id-6256">
+              <div className="proscons-1-478440 pos-abs" id="id-6257">
+                <span className="proscons-1-478440-0">
+                  {" "}
+                  <br />{" "}
+                </span>
+                <span className="proscons-1-478440-1">
+                  {"Cons: "} <br />{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-4-1-216032 pos-abs" id="id-10110">
+          <div className="navigation-bar-1-638510 pos-abs" id="id-3712">
+            <div className="rectangle-2-1-186472 pos-abs" id="id-223"></div>
+            <div className="profile-1-243608 pos-abs" id="id-94231">
+              <div className="freeprofile1439-1-729372 pos-abs" id="id-3027">
+                <div
+                  className="nodeBg-3027 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                  id="id-bg-3027"
+                >
+                  {" "}
+                </div>
+                <div
+                  className="nodeBg-3027 pos-abs image-div bg-no-repeat fill-parent bg-cover"
+                  id="id-bg-3027"
+                >
+                  {" "}
+                </div>
+              </div>
+            </div>
+            <div className="wrench-1-22784 pos-abs" id="id-94169">
+              <div className="wrench-1-722848 pos-abs" id="id-2511">
+                <div className="ellipse-1-1-473355 pos-abs" id="id-258"></div>
+                <div className="rectangle-9-1-58305 pos-abs" id="id-259"></div>
+                <div
+                  className="rectangle-10-1-209625 pos-abs"
+                  id="id-2510"
+                ></div>
+              </div>
+            </div>
+            <div className="home-1-302400 pos-abs" id="id-10111">
+              <div className="home-icon-1-52020 pos-abs" id="id-257">
+                <div className="rectangle-7-1-312840 pos-abs" id="id-254"></div>
+                <div className="rectangle-8-1-157320 pos-abs" id="id-255"></div>
+                <div className="polygon-1-1-180695 pos-abs" id="id-256">
+                  <div
+                    className="nodeBg-256 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-256"
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className="stock-1-19089 pos-abs" id="id-94230">
+              <div className="stock-1-94544 pos-abs" id="id-2516">
+                <div className="line-15-1-105678 pos-abs" id="id-2514"></div>
+                <div className="line-13-1-366261 pos-abs" id="id-2512">
+                  <div
+                    className="nodeBg-2512 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-2512"
+                  ></div>
+                </div>
+                <div className="line-14-1-150230 pos-abs" id="id-2513">
+                  <div
+                    className="nodeBg-2513 pos-abs pos-init fill-parent bg-contain bg-no-repeat image-div"
+                    id="id-bg-2513"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("dualite-root")).render(<App />);
