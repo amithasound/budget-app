81c417d8a987b684f88c3b1076ad29ded3ea3359
amithasound <70346133+amithasound@users.noreply.github.com>
2024-04-07 11:09:39 -0400

    change my css

--- react-copilot-test/budget-buddy/src/InvestmentGoals.js
+++ react-copilot-test/budget-buddy/src/InvestmentGoals.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { List, ListItem, ListItemIcon, ListItemButton } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Divider } from '@mui/material';
 
 const InvestmentGoals = () => {
      const navigate = useNavigate();
@@ -30,45 +31,37 @@ const InvestmentGoals = () => {
        <div>
                    <h1>Learn about some popular investment accounts!</h1>
                    <List>
-                       <ListItem>
+                       <ListItem sx={{ border: '1px solid gray', borderRadius: '5px', marginBottom: '0px', padding: '0px', margin: '0px' }}>
                            <ListItemButton onClick={navigateToIRA}>
                                IRA
-                               <ListItemIcon>
-                                   <ArrowForwardIcon />
-                              </ListItemIcon>
+                                <ListItemIcon> <ArrowForwardIcon /> </ListItemIcon>
                            </ListItemButton>
                        </ListItem>
-                       <ListItem>
+
+                       <ListItem sx={{ border: '1px solid gray', borderRadius: '5px', marginBottom: '0px', padding: '0px', margin: '0px' }}>
                            <ListItemButton onClick={navigateToK401}>
                                401K
-                               <ListItemIcon>
-                                     <ArrowForwardIcon />
-                                </ListItemIcon>
+                              <ListItemIcon> <ArrowForwardIcon /> </ListItemIcon>
                            </ListItemButton>
                        </ListItem>
-                       <ListItem>
+                       <ListItem sx= {{ border: '1px solid gray', borderRadius: '5px', marginBottom: '0px', padding: '0px', margin: '0px' }}>
                            <ListItemButton onClick={navigateToRothIRAinfo}>
                                Roth IRA
-                               <ListItemIcon>
-                                    <ArrowForwardIcon />
-                                </ListItemIcon>
+                              <ListItemIcon> <ArrowForwardIcon /> </ListItemIcon>
                            </ListItemButton>
                        </ListItem>
-                       <ListItem>
+                       <ListItem sx={{ border: '1px solid gray', borderRadius: '5px', marginBottom: '0px' }}>
                            <ListItemButton onClick={navigateToRoth401K}>
                                Roth 401K
-                                <ListItemIcon>
-                                  <ArrowForwardIcon />
-                                 </ListItemIcon>
+                                <ListItemIcon> <ArrowForwardIcon /> </ListItemIcon>
                            </ListItemButton>
                        </ListItem>
-                       <ListItem>
+                       <ListItem sx={{ border: '1px solid gray', borderRadius: '5px', marginBottom: '0px' }}>
                               <ListItemButton onClick={navigateTo529}>
-                                                      529
-                                  <ListItemIcon>
-                                           <ArrowForwardIcon />
-                                   </ListItemIcon>
+                                   529
+                                  <ListItemIcon> <ArrowForwardIcon /> </ListItemIcon>
                                </ListItemButton>
+
                          </ListItem>
                    </List>
                </div>
